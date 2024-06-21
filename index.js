19809d34021e35579c9c3031f52c46a333478e7f
Levy Chemelil <levy.chelal@student.moringaschool.com>
2024-06-21 17:11:48 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-// Write your solution here!
+// index.js
+
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+function appendCat(name) {
+  let newArray = [...cats, name];
+  return newArray;
+}
+
+function prependCat(name) {
+  let newArray = [name, ...cats];
+  return newArray;
+}
+
+function removeLastCat() {
+  let newArray = cats.slice(0, -1);
+  return newArray;
+}
+
+function removeFirstCat() {
+  let newArray = cats.slice(1);
+  return newArray;
+}
+
+module.exports = {
+  cats,
+  destructivelyAppendCat,
+  destructivelyPrependCat,
+  destructivelyRemoveLastCat,
+  destructivelyRemoveFirstCat,
+  appendCat,
+  prependCat,
+  removeLastCat,
+  removeFirstCat,
+};
